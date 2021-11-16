449f95d356d3e37563e94c9ff4d7c9dd54be34a6
Alexander <v0xt.alex@gmail.com>
2021-11-16 16:27:48 +0300

    Replace loading spinner with running zombie anim

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -2,35 +2,16 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        zIndex: 2,
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "rgba(255, 255, 255, 0.5)",
-      }}
-    >
       <div
         style={{
-          position: "absolute",
-          zIndex: 3,
-          top: "50%",
-          left: "50%",
-          width: "100px",
-          height: "50px",
-          marginLeft: "-50px",
-          marginTop: " -25px",
-          textAlign: "center",
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+          padding: '10px',
         }}
-      >
-        <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-      </div>
-    </div>
+        className="zombie-loading"
+      />
   );
 }
 
